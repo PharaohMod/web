b9ffbccd37939ad6937c5dacf66c28b52108e884
Pharaoh Mod <70447547+PharaohMod@users.noreply.github.com>
2022-08-08 19:51:23 +0630

    Create mykey.js

---
+++ mykey.js
@@ -0,0 +1,16 @@
+<script>
+document.getElementById('text').addEventListener("click", loadKey);
+function loadKey(){
+        var url = window.location.href ,
+  params = url.split('?')[1].split('&'),
+        data = {}, tmp;1
+    for (var i = 0, l = params.length; i < l; i++) {
+         tmp = params[i].split('=');
+         data[tmp[0]] = tmp[1];
+    }
+var raw = "👇Copy👇</br>";
+ raw  += data.kdevp;
+    document.getElementById("tex").innerHTML =raw;
+    //window.location.assign(raw);
+   };
+   </script>
